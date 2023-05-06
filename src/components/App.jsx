640553cb42250080a7b0f373aa30a9016bd08855
Irgey <116591589+Irgey@users.noreply.github.com>
2023-05-07 01:24:20 +0200

    background, random wishes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,43 @@
 import { useEffect, useState } from 'react';
-
+import Clock from 'react-live-clock';
+import { morningWishes } from 'data/morningWishes';
+import { dayWishes } from 'data/dayWishes';
+import { eveningWishes } from 'data/eveningWishes';
+import { nightWishes } from 'data/nightWishes';
 export const App = () => {
   const [wish, setWish] = useState('');
   const date = new Date();
   console.dir(date);
   const hour = date.getHours();
   console.log(hour);
+  const getRandomWish = wishesArr => {
+    const rndIndex = Math.floor(Math.random() * wishesArr.length);
+    return wishesArr[rndIndex];
+  };
   useEffect(() => {
-    if (hour >= 5 && hour <= 10) {
-      setWish('Good morning 🌞. Wish you a good day!');
-    } else if (hour > 10 && hour <= 18) {
-      setWish(
-        'Good day 🌞. I hope this message brightens your day and makes it a little better. You are very important to many people and I hope your day is filled with happiness, laughter and opportunities. Go and take a day! Wishing you a great day ahead, may you achieve whatever you have planned for today.!'
-      );
-    } else if (hour > 18 && hour <= 21) {
-      setWish('Good Evening 🌆! I hope you had a good and productive day.');
+    if (hour >= 5 && hour <= 9) {
+      setWish(getRandomWish(morningWishes));
+      document.body.style.backgroundImage =
+        'url(https://images.pexels.com/photos/849403/pexels-photo-849403.jpeg?auto=compress)';
+      document.body.style.color = ' #ffffff';
+    } else if (hour > 9 && hour <= 17) {
+      setWish(getRandomWish(dayWishes));
+      document.body.style.backgroundImage =
+        'url(https://images.pexels.com/photos/5015450/pexels-photo-5015450.jpeg?auto=compress)';
+      document.body.style.color = ' #000000';
+    } else if (hour > 17 && hour <= 21) {
+      setWish(getRandomWish(eveningWishes));
+      document.body.style.backgroundImage =
+        'url(https://images.pexels.com/photos/14803699/pexels-photo-14803699.jpeg?auto=compress)';
     } else if (hour > 21) {
-      setWish(
-        'It`s night already 🌉! Wishing you a wonderful night`s sleep 🌙✨.'
-      );
+      setWish(getRandomWish(nightWishes));
     } else {
       setWish(
         'Oh, it`s late night! I hope you go to sleep as soon as possible 🌙✨.'
       );
+      document.body.style.backgroundImage =
+        'url(https://images.pexels.com/photos/9991623/pexels-photo-9991623.jpeg?auto=compress)';
+      document.body.style.color = ' #e6dcdc';
     }
   }, [hour]);
   return (
@@ -30,12 +45,13 @@ export const App = () => {
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101',
       }}
     >
+      <Clock format={'HH:mm:ss'} ticking={true} />
       {wish}
     </div>
   );
